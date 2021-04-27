0268ace251f4a8ea7d4a7e0c6afc13a34f4ac6fd
SwarzChen <aaaa102234@gmail.com>
2021-04-27 16:06:51 +0800

    [main] finish menu animation

---
+++ src/components/hamburgerMenu/menuToggle.jsx
@@ -0,0 +1,60 @@
+import { motion } from "framer-motion";
+import React from "react";
+import './menuToggle.css'
+
+const variantTop = {
+	closed: { d: "M 2 2.5 L 20 2.5", stroke: "hsl(0, 0%, 100%)" },
+	open: { d: "M 3 16.5 L 17 2.5", stroke: "hsl(0, 0%, 18%)" }
+};
+
+const variantMiddle = {
+	closed: { opacity: 1 },
+	open: { opacity: 0 }
+}
+
+const variantButtom = {
+	closed: { d: "M 2 16.346 L 20 16.346", stroke: "hsl(0, 0%, 100%)" },
+	open: { d: "M 3 2.5 L 17 16.346", stroke: "hsl(0, 0%, 18%)" }
+}
+
+const transition = { duration: 0.3 };
+
+const MenuToggle = ({ toggle, isOpen }) => {
+	return (
+		<div className="Button" onClick={toggle}>
+			<svg width="23" height="23" viewBox="0 0 23 23">
+				<motion.path
+					fill="transparent"
+					strokeLinecap="round"
+					strokeWidth="3"	
+					animate={isOpen ? "open" : "closed"}
+					initial={false}
+					variants={variantTop}
+					transition={transition}
+				/>
+				<motion.path
+					fill="transparent"
+					strokeLinecap="round"
+					strokeWidth="3"
+					d="M 2 9.423 L 20 9.423"
+					stroke="hsl(0, 0%, 100%)"
+					animate={isOpen ? "open" : "closed"}
+					initial={false}
+					variants={variantMiddle}
+					transition={transition}
+				/>
+				<motion.path
+					fill="transparent"
+					strokeLinecap="round"
+					strokeWidth="3"
+					animate={isOpen ? "open" : "closed"}
+					initial={false}
+					variants={variantButtom}
+					transition={transition}
+				/>
+			</svg>
+		</div>
+	);
+}
+
+export default MenuToggle;
