93981148e217da176aee8f5087f5ea20e9fc5142
SwarzChen <aaaa102234@gmail.com>
2021-04-27 16:06:22 +0800

    [main] finish structuring

--- src/components/hamburgerMenu/index.jsx
+++ src/components/hamburgerMenu/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./index.css"
+import {motion} from 'framer-motion'
+import MenuToggle from './menuToggle'
 
-const HamburgerMenu = () => {
+const HamburgerMenu = (props) => {
+    const menuVariants = {
+        open: {
+            transform: "translateX(3%)"
+        },
+        closed: {
+            transform: "translateX(103%)"
+        }
+    };
 
+    const menuTransition = {
+        type: "spring",
+        duration: 1,
+        stiffness: 33,
+        delay: 0.1
+    };
+
+    const [isOpen, setOpen] = useState(false);
+
+    const ToggleMenu = () => {
+        setOpen(!isOpen)
+    }
+
+    return (
+        <div className="HamburgerMenuContainer">
+            <MenuToggle toggle={ToggleMenu} isOpen={isOpen}></MenuToggle>
+            <motion.div 
+                className="MenuContainer"
+                initial={false}
+                transition={menuTransition}
+                animate={isOpen ? "open" : "closed"}
+                variants={menuVariants}
+            />
+        </div>
+    );
 }
 
 export default HamburgerMenu;
