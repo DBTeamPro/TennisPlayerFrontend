393eff18bc49960635bc8574aead21141d681e9e
SwarzChen <aaaa102234@gmail.com>
2021-05-19 14:57:58 +0800

    [main] add activity page

---
+++ src/pages/activity.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Activity = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Activity
+        </div>
+    );
+}
+
+export default Activity;
