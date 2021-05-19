8b56e3c0a829b6fb125459d497e6d70a340a0144
SwarzChen <aaaa102234@gmail.com>
2021-05-19 14:58:24 +0800

    [main] add information page

---
+++ src/pages/information.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+const Information = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Information
+        </div>
+    );
+}
+
+export default Information;
