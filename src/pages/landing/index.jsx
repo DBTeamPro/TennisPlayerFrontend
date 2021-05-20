450d4846c596a1e1045b0e2166def85d98a37e1c
SwarzChen <aaaa102234@gmail.com>
2021-05-20 23:49:05 +0800

    [main] add landing page

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/navbar/index'
+
+const Landing = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
+
+export default Landing;
