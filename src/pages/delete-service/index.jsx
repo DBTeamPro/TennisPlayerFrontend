50ffc7cb6b02c22e6b32b277f0e9c19c2b2d395d
SwarzChen <aaaa102234@gmail.com>
2021-05-20 23:48:45 +0800

    [main] change delete service

---
+++ src/pages/delete-service/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+const Delete = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Delete
+        </div>
+    );
+}
+
+export default Delete;
