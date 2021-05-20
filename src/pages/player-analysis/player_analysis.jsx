06ffdfce8cceb47d2cbfeb8a63736761d602f9d3
SwarzChen <aaaa102234@gmail.com>
2021-05-20 23:50:15 +0800

    [main] add player service

---
+++ src/pages/player-analysis/player_analysis.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+const PlayerAnalysis = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Activity
+        </div>
+    );
+}
+
+export default PlayerAnalysis;
