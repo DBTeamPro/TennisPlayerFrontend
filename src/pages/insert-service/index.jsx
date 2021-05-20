cf9f6614fa6035d9a4bc5da9cbe71ddecc05181f
SwarzChen <aaaa102234@gmail.com>
2021-05-20 23:48:56 +0800

    [main] add insert service

---
+++ src/pages/insert-service/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+const Insert = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Insert
+        </div>
+    );
+}
+
+export default Insert;
