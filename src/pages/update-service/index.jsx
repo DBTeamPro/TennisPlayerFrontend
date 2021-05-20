fa554b5888168a7fb957d840bf456d2707740563
SwarzChen <aaaa102234@gmail.com>
2021-05-20 23:50:38 +0800

    [main] add update service

---
+++ src/pages/update-service/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+
+const Update = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            Update
+        </div>
+    );
+}
+
+export default Update;
