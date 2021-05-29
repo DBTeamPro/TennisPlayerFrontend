b5a527500aea7985f888bebd3a5b27f3ec511ee1
SwarzChen <aaaa102234@gmail.com>
2021-05-29 17:36:06 +0800

    [main] create big 3 service

---
+++ src/components/player-switchbox/big3.jsx
@@ -0,0 +1,96 @@
+import {React, useState, useEffect} from 'react'
+import './big3.css'
+import GraphBox from '../graphbox/index'
+import axios from 'axios'
+
+const Big3 = () => {
+    const [data, setData] = useState('')
+    const url = 'http://localhost:5000/'
+
+    const getData = async () => {
+        try {
+            const response = await axios.get(`${url}player/big3`)
+            setData(response.data)
+        }
+        catch (error){
+            console.error(`Error: ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+    
+    let playerAge = []
+    let playerCountry = []
+    let numOfChampG = []
+    let numOfChampM = []
+    let numOfChampA = []
+    let winLose = []
+
+    if(data.length > 0){
+        playerAge = data.map((ele) => ele.age)
+        playerCountry = data.map((ele) => ele.country)
+        numOfChampG = data.map((ele) => ele.champ_G)
+        numOfChampM = data.map((ele) => ele.champ_M)
+        numOfChampA = data.map((ele) => ele.champ_A)
+        winLose = data.map((ele) => ele.win_lose)
+    }
+
+    return(
+        <div className="big3-container">
+            <div className="wallpaper">
+                <div className="row1">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+                <div className="row2">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+                <div className="row3">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+                <div className="row4">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+                <div className="row5">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+                <div className="row6">
+                    <div className="col1"></div>
+                    <div className="col2"></div>
+                    <div className="col3"></div>
+                    <div className="col4"></div>
+                    <div className="col5"></div>
+                    <div className="col6"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Big3;
