620b7aa6ff2f96cc7404f497d019518cad810f07
SwarzChen <aaaa102234@gmail.com>
2021-05-29 16:22:49 +0800

    [main] create player analysis

--- src/pages/player-analysis/index.jsx
+++ src/pages/player-analysis/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
+import SwitchBox from '../../components/switchbox/match_switchbox'
+import './index.css'
 
 const PlayerAnalysis = () => {
+    const cata = ['比賽數量', '比賽時間', '持拍慣用手', '球員身高', '三巨頭']
+
     return (
-        <div>
+        <div className="player-analysis-container">
             <Navbar></Navbar>
-            Activity
+            <SwitchBox 
+                t1={cata[0]}
+                t2={cata[1]}
+                t3={cata[2]}
+                t4={cata[3]}
+                t5={cata[4]}
+            ></SwitchBox>
         </div>
     );
 }
