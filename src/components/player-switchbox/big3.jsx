926831272b8d053edc9393bd01d7bef5fa48957f
SwarzChen <aaaa102234@gmail.com>
2021-05-30 15:08:35 +0800

    [main] finish big3 service

--- src/components/player-switchbox/big3.jsx
+++ src/components/player-switchbox/big3.jsx
@@ -40,53 +40,56 @@ const Big3 = () => {
     return(
         <div className="big3-container">
             <div className="wallpaper">
+                <img src="image/big3.jpg" width="100%" height="100%" alt="#" />
+            </div>
+            <div className="score-container">
                 <div className="row1">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">AGE</div>
+                    <div className="col2">{data.length > 0 && playerAge[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && playerAge[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && playerAge[0]}</div>
                 </div>
                 <div className="row2">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">COUNTRY</div>
+                    <div className="col2">{data.length > 0 && playerCountry[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && playerCountry[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && playerCountry[0]}</div>
                 </div>
                 <div className="row3">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">CHAMPG</div>
+                    <div className="col2">{data.length > 0 && numOfChampG[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && numOfChampG[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && numOfChampG[0]}</div>
                 </div>
                 <div className="row4">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">CHAMPM</div>
+                    <div className="col2">{data.length > 0 && numOfChampM[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && numOfChampM[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && numOfChampM[0]}</div>
                 </div>
                 <div className="row5">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">CHAMPA</div>
+                    <div className="col2">{data.length > 0 && numOfChampA[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && numOfChampA[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && numOfChampA[0]}</div>
                 </div>
                 <div className="row6">
-                    <div className="col1"></div>
-                    <div className="col2"></div>
+                    <div className="col1">WIN-LOSE</div>
+                    <div className="col2">{data.length > 0 && winLose[2]}</div>
                     <div className="col3"></div>
-                    <div className="col4"></div>
+                    <div className="col4">{data.length > 0 && winLose[1]}</div>
                     <div className="col5"></div>
-                    <div className="col6"></div>
+                    <div className="col6">{data.length > 0 && winLose[0]}</div>
                 </div>
             </div>
         </div>
