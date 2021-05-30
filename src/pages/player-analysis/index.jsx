229f77ef4a59dd9453237b8a95390483d8d946f1
SwarzChen <aaaa102234@gmail.com>
2021-05-30 15:09:46 +0800

    [main] new page activate

--- src/pages/player-analysis/index.jsx
+++ src/pages/player-analysis/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
-import SwitchBox from '../../components/match-switchbox/match_switchbox'
+import PlayerSwitchBox from '../../components/player-switchbox/player_switchbox'
 import './index.css'
 
 const PlayerAnalysis = () => {
@@ -9,13 +9,15 @@ const PlayerAnalysis = () => {
     return (
         <div className="player-analysis-container">
             <Navbar></Navbar>
-            <SwitchBox 
-                t1={cata[0]}
-                t2={cata[1]}
-                t3={cata[2]}
-                t4={cata[3]}
-                t5={cata[4]}
-            ></SwitchBox>
+            <div className="content">
+                <PlayerSwitchBox 
+                    t1={cata[0]}
+                    t2={cata[1]}
+                    t3={cata[2]}
+                    t4={cata[3]}
+                    t5={cata[4]}
+                ></PlayerSwitchBox>
+            </div>
         </div>
     );
 }
