b7fd0f81cf097fe6c4189dd7249e51f03afd5ede
SwarzChen <aaaa102234@gmail.com>
2021-06-03 02:08:52 +0800

    [main]  activate new landing routing path

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MatchAnalysis from './pages/match-analysis/index'
 import PlayerAnalysis from './pages/player-analysis/index'
 import SearchService from './pages/search-service/index'
+import SmallMatch from './pages/small-match/index'
+import About from './pages/about/index'
 
 function App() {
 return (
 	<BrowserRouter >
 		<div className="AppContainer">
 			<Switch>
-				<Route exact path="/" component={MatchAnalysis}/>
+				<Route exact path="/match" component={MatchAnalysis}/>
 				<Route path="/player" component={PlayerAnalysis}/>
 				<Route path="/search" component={SearchService}/>
+				<Route path="/small-match" component={SmallMatch}/>
+				<Route path="/" component={About}/>
 			</Switch>
 		</div>
 	</BrowserRouter>
